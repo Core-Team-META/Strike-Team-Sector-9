1dd32c4c13a921d6f35e2d430e15966a573f18dd
Rolok <imrolok@yahoo.com>
2021-06-18 12:49:24 -0700

    News set to direct to our player discord

--- NewSniperAlley/Data/Scripts/RichText_EXAMPLE.lua
+++ NewSniperAlley/Data/Scripts/RichText_EXAMPLE.lua
@@ -26,8 +26,8 @@ Text that has an image (<image sword>) which I think is very nice!</font>
 local text2 = [[
 <size 25><font play_bold>4/06/2021</font></size>
 <image gradient -1 3 #FF3300>
-<panel -1 #FF330022>Welcome to Strike Team: Sector 9!</panel>
-<panel -1 #FF330022>We hope you enjoy this wide open map.</panel>
+<panel -1 #FF330022>For the most recent news check out the Strike Team channel on the player discord</panel>
+<panel -1 #FF330022>https://discord.gg/Cp7VEsqg</panel>
 
 ]]
 
