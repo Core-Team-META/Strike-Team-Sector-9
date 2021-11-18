4374103ec7b2aa917ab8aa2b812ea13a66fc61f3
Benjamin Staneck <bstaneck@manticoregames.com>
2021-11-18 19:37:09 +0100

    Fix syntax error

--- NewSniperAlley/Data/Scripts/HangingWireAnimator.lua
+++ NewSniperAlley/Data/Scripts/HangingWireAnimator.lua
@@ -6,11 +6,12 @@ local END_ROTATION = script:GetCustomProperty("EndRotation")
 -- <Rotation> LerpRotation (Rotation, Rotation, number)
 -- Lerps Rotation properties by using Quaternions
 function LerpRotation(from, to, t)
-    local fromQuat = Quaternion.New(from)
-    local toQuat = Quaternion.New(to)
-    return Rotation.New(Quaternion.Slerp(fromQuat, toQuat, t))
+	local fromQuat = Quaternion.New(from)
+	local toQuat = Quaternion.New(to)
+	return Rotation.New(Quaternion.Slerp(fromQuat, toQuat, t))
 end
 
 function Tick(deltaTime)
-    OBJ:SetRotation(Rotation) LerpRotation(START_VALUE_FROM, END_VALUE, t))
+	OBJ:SetRotation(Rotation)
+	LerpRotation(START_VALUE_FROM, END_VALUE, t)
 end
